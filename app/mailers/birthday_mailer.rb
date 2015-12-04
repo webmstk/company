552ec8f21e35aba8f0b562ca4b40df8636e9ce67
webmstk <webmstk@mail.ru>
2015-12-04 09:07:23 +0300

    add email to emails list

--- app/mailers/birthday_mailer.rb
+++ app/mailers/birthday_mailer.rb
@@ -6,7 +6,7 @@ class BirthdayMailer < ApplicationMailer
   #   en.birthday_mailer.notify.subject
   #
   def notify
-    emails = %w(eta@svarbi.ru)
+    emails = %w(eta@svarbi.ru lubov@svarbi.ru)
 
     @persons = Person.birthday
     if @persons.any?
